23ce400a33b5b53d654859f69293566468011eae
Paymushkin <paymushkin_a@bk.ru>
2025-10-09 13:42:46 +0300

    Deploy to GitHub Pages - 2025-10-09T10:42:45.089Z

---
+++ _nuxt/fO1cBR2n.js
@@ -0,0 +1,1 @@
+import{e as s,c as n,o,x as t}from"./X6eRKcPf.js";const r={class:"min-h-screen"},_=s({__name:"fullscreen",setup(a){return(e,c)=>(o(),n("div",r,[t(e.$slots,"default")]))}});export{_ as default};
